25a427c02f5b7171ab4ef4d1003bc7abd5ef957c
rishi monohur <rishi.monohur15@gmail.com>
2023-10-15 12:11:58 +0400

    add CRUD for list

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,46 @@
+class ListsController < ApplicationController
+
+  before_action :set_list, only: [:show, :destroy, :edit, :update]
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    @list.save
+    redirect_to lists_path
+  end
+
+  def destroy
+    @list.destroy
+    redirect_to lists_path, status: :see_other
+  end
+
+
+  def edit
+  end
+
+  def update
+    @list.update(list_params)
+    redirect_to list_path(@list)
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :lists, only: [:index, :show, :new, :create, :destroy, :edit, :update] do
+
+  end
 end
